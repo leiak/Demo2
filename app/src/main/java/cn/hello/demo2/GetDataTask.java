14db15b9ab43bb9fc7acf175ef94d3194f5e9ff2
lei <fang_diyige@163.com>
2017-09-06 11:16:19 +0800

    pulltorefresh create

---
+++ app/src/main/java/cn/hello/demo2/GetDataTask.java
@@ -0,0 +1,58 @@
+package cn.hello.demo2;
+
+import android.os.AsyncTask;
+import android.widget.ArrayAdapter;
+
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.Mode;
+
+import java.util.LinkedList;
+
+/**
+ * Created by Administrator on 2017/9/6.
+ */
+
+public class GetDataTask extends AsyncTask<Void, Void, Void> {
+
+
+    private PullToRefreshListView mPullRefreshListView;
+    private ArrayAdapter<String> mAdapter;
+    private LinkedList<String> mListItems;
+
+    public GetDataTask(PullToRefreshListView listView,
+                       ArrayAdapter<String> adapter,LinkedList<String> listItems) {
+        // TODO 自动生成的构造函数存根
+        mPullRefreshListView = listView;
+        mAdapter = adapter;
+        mListItems = listItems;
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        //模拟请求
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        // TODO 自动生成的方法存根
+        super.onPostExecute(result);
+        //得到当前的模式
+        Mode mode = mPullRefreshListView.getCurrentMode();
+        if(mode == Mode.PULL_FROM_START) {
+            mListItems.addFirst("这是刷新出来的数据");
+        }
+        else {
+            mListItems.addLast("这是刷新出来的数据");
+        }
+        // 通知数据改变了
+        mAdapter.notifyDataSetChanged();
+        // 加载完成后停止刷新
+        mPullRefreshListView.onRefreshComplete();
+
+    }
+}
--- app/src/main/java/cn/hello/demo2/PullToRefreshActivity.java
+++ app/src/main/java/cn/hello/demo2/PullToRefreshActivity.java
@@ -1,7 +1,19 @@
 package cn.hello.demo2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateUtils;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.Mode;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.handmark.pulltorefresh.library.extras.SoundPullEventListener;
+
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class PullToRefreshActivity extends AppCompatActivity {
 
@@ -9,5 +21,91 @@ public class PullToRefreshActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pull_to_refresh);
+        initView();
+
+        //一打开应用就自动刷新，下面语句可以写到刷新按钮里面
+        mPullRefreshListView.setRefreshing(true);
+        //new GetDataTask(mPullRefreshListView, mAdapter, mListItems).execute();
+        //mPullRefreshListView.setRefreshing(false);
+    }
+
+    //一个可以下拉刷新的listView对象
+    private PullToRefreshListView mPullRefreshListView;
+    //普通的listview对象
+    private ListView actualListView;
+    //添加一个链表数组，来存放string数组，这样就可以动态增加string数组中的内容了
+    private LinkedList<String> mListItems;
+    //给listview添加一个普通的适配器
+    private ArrayAdapter<String> mAdapter;
+
+
+
+    private void initView() {
+        initPTRListView();
+        initListView();
+    }
+
+    /**
+     * 设置下拉刷新的listview的动作
+     */
+    private void initPTRListView() {
+        mPullRefreshListView = (PullToRefreshListView) findViewById(R.id.pull_refresh_list);
+        //设置拉动监听器
+        mPullRefreshListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
+
+            @Override
+            public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+                //设置下拉时显示的日期和时间
+                String label = DateUtils.formatDateTime(getApplicationContext(), System.currentTimeMillis(),
+                        DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_ALL);
+
+                // 更新显示的label
+                refreshView.getLoadingLayoutProxy().setLastUpdatedLabel(label);
+                // 开始执行异步任务，传入适配器来进行数据改变
+                new GetDataTask(mPullRefreshListView, mAdapter,mListItems).execute();
+            }
+        });
+
+        // 添加滑动到底部的监听器
+        mPullRefreshListView.setOnLastItemVisibleListener(new PullToRefreshBase.OnLastItemVisibleListener() {
+
+            @Override
+            public void onLastItemVisible() {
+                Toast.makeText(getApplication(), "已经到底了", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //mPullRefreshListView.isScrollingWhileRefreshingEnabled();//看刷新时是否允许滑动
+        //在刷新时允许继续滑动
+        mPullRefreshListView.setScrollingWhileRefreshingEnabled(true);
+        //mPullRefreshListView.getMode();//得到模式
+        //上下都可以刷新的模式。这里有两个选择：Mode.PULL_FROM_START，Mode.BOTH，PULL_FROM_END
+        mPullRefreshListView.setMode(Mode.BOTH);
+
+        /**
+         * 设置反馈音效
+         */
+        SoundPullEventListener<ListView> soundListener = new SoundPullEventListener<ListView>(this);
+        //soundListener.addSoundEvent(State.PULL_TO_REFRESH, R.raw.pull_event);
+        //soundListener.addSoundEvent(State.RESET, R.raw.reset_sound);
+        //soundListener.addSoundEvent(State.REFRESHING, R.raw.refreshing_sound);
+        mPullRefreshListView.setOnPullEventListener(soundListener);
+    }
+
+    /**
+     * 设置listview的适配器
+     */
+    private void initListView() {
+        //通过getRefreshableView()来得到一个listview对象
+        actualListView = mPullRefreshListView.getRefreshableView();
+
+        String []data = new String[] {"android","ios","wp","java","c++","c#"};
+        mListItems = new LinkedList<String>();
+        //把string数组中的string添加到链表中
+        mListItems.addAll(Arrays.asList(data));
+
+        mAdapter = new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_list_item_1, mListItems);
+        actualListView.setAdapter(mAdapter);
     }
 }
